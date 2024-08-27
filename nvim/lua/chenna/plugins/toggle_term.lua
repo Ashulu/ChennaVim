6340bfd09619b7946feba806d3ad95ee5b8ff926
Ashulu <ashish.chenna05@gmail.com>
2024-08-27 00:35:43 -0400

    made a few tweaks to toggle term config to better suit me

--- nvim/lua/chenna/plugins/toggle_term.lua
+++ nvim/lua/chenna/plugins/toggle_term.lua
@@ -1,13 +1,15 @@
---local toggle_setup, toggle = pcall(require, "toggleterm")
+-- local toggle_setup, toggle = pcall(require, "toggleterm")
 --if not toggle_setup then
 --  return
 --end
 
-require"toggleterm".setup({
-  size = 13,
+require("toggleterm").setup({
+  size = 13, 
   open_mapping = [[<c-\>]],
+  hide_numbers = true,
   shade_filetypes = {},
-  shade_terminals = true,
+  autochdir = false,
+  shade_terminals = false,
   shading_dactor = 1,
   start_in_insert = true,
   persist_size = true,
